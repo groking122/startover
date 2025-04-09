286aa9267751688793ed5b521dc66d451bcc428a
groking122 <sakissakakis21@gmail.com>
2025-04-10 01:24:41 +0300

    Fix Next.js config: update deprecated options and resolve package conflicts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
@@ -15,11 +14,11 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   // Ensure we handle the build of specific packages properly
-  transpilePackages: ['lucid-cardano', '@cardano-sdk/cip30', '@cardano-sdk/core', '@cardano-sdk/util'],
+  transpilePackages: ['@cardano-sdk/cip30', '@cardano-sdk/core', '@cardano-sdk/util'],
   // Configure external packages that shouldn't be bundled
   experimental: {
     // This is necessary to handle cardano libraries that access window
-    serverComponentsExternalPackages: ['lucid-cardano']
+    serverExternalPackages: ['lucid-cardano']
   },
   // Ensure we do proper handling of lucid-cardano's usage of topLevelAwait
   webpack: (config) => {
